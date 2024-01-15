2196b15f6ab4c8264c33c6d5ad673b532e1afe01
David Benkner <d.benkner@gmail.com>
2024-01-15 16:29:44 -0500

    start of razor

---
+++ BulkyWebRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BulkyWebRazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        } 
+        public DbSet<Category>  Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, DisplayOrder = 1, Name = "History" },
+                new Category { Id =2, DisplayOrder =2 , Name ="SciFi"},
+                new Category { Id =3, DisplayOrder = 3, Name = "SelfHelp"}
+                );
+        }
+    }
+}
